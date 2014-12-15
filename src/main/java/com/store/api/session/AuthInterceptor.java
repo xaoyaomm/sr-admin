2d1ae23cec586bb8e8003ed23764abcb55b8bce3
vincent <xaoyaomm@aliyun.com>
2014-12-16 01:10:15 +0800

    修改

---
+++ src/main/java/com/store/api/session/AuthInterceptor.java
@@ -0,0 +1,59 @@
+package com.store.api.session;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.store.api.common.Constant;
+import com.store.api.mongo.entity.User;
+import com.store.api.utils.JsonUtils;
+import com.store.api.utils.Utils;
+
+public class AuthInterceptor extends HandlerInterceptorAdapter {
+
+    private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        if (handler.getClass().isAssignableFrom(HandlerMethod.class)) {
+            response.setContentType("text/plain;charset=UTF-8");
+            // 没有声明需要权限
+            StringBuffer sbUrl = new StringBuffer();
+            Enumeration<String> en = request.getParameterNames();// 请求参数-值
+            sbUrl.append(request.getRequestURI().toString());
+            for (int i = 0; en.hasMoreElements(); i++) {
+                String arg = en.nextElement().toString();
+                if (i == 0) {
+                    sbUrl.append("?");
+                } else {
+                    sbUrl.append("&");
+                }
+                sbUrl.append(arg + "=" + request.getParameterValues(arg)[0]);
+            }
+
+            LOG.error("{}", sbUrl.toString());
+            Object userName = request.getSession().getAttribute("name");
+            if (null == userName) {
+                if ("XMLHttpRequest".equalsIgnoreCase(request.getHeader("X-Requested-With")) || request.getParameter("ajax") != null) {
+
+                    String msg = "{\"statusCode\":\"301\", \"message\":\"会话超时，请重新登录\"}";
+                    response.getWriter().write(msg);
+                    return false;
+                } else {
+                    return false;
+                }
+            } else
+                return true;
+
+        } else
+            return true;
+    }
+
+}
